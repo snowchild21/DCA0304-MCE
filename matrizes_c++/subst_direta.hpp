3be817bc9f2aeddc5d44e52ec65279af468c46e2
Jéssica Oliveira <jessicamelestudante@gmail.com>
2025-09-19 19:27:47 -0300

    branch main limpinha para usar na AVA 1

--- matrizes_c++/subst_direta.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef SUBST_DIRETA_HPP
-#define SUBST_DIRETA_HPP
-
-#include <iostream>
-#include <vector>
-
-std::vector<double> substituicaoDireta(const std::vector<double>& A, const std::vector<double>& b, int n);
-#endif 
--- matrizes_c++/subst_reversa.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef SUBST_REVERSA_HPP
-#define SUBST_REVERSA_HPP
-
-#include <iostream>
-#include <vector>
-
-std::vector<double> substituicaoReversa(const std::vector<double>& A, const std::vector<double>& b, int n);
-#endif
